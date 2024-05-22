e3985f4c68f79fd95217c21dba6b72ee7cc4463b
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-05-21 23:42:57 -1000

    Add async handler to remove try catch block

--- controllers/submissionController.js
+++ controllers/submissionController.js
@@ -1,34 +1,36 @@
+import asyncHandler from "express-async-handler"
+
 const isEmpty = (obj) => {
 	return JSON.stringify(obj) === "{}"
 }
 
-const getSubmission = (req, res) => {
+const getSubmission = asyncHandler(async (req, res) => {
 	res.status(200).send("Get Submissions")
-}
+})
 
-const getSubmissionById = (req, res) => {
+const getSubmissionById = asyncHandler(async (req, res) => {
 	res.status(200).send(`Get Submission with id ${req.params.id}`)
-}
+})
 
-const createSubmission = (req, res) => {
+const createSubmission = asyncHandler(async (req, res) => {
 	if (isEmpty(req.body)) {
-        res.status(400).send("No data provided")
-    }
+		res.status(400).send("No data provided")
+	}
 
 	res.status(200).send("Create Submission")
-}
+})
 
-const updateSubmission = (req, res) => {
+const updateSubmission = asyncHandler(async (req, res) => {
 	if (isEmpty(req.body)) {
-        res.status(400).send("No data provided")
-    }
+		res.status(400).send("No data provided")
+	}
 
 	res.status(200).send(`Update Submission with id ${req.params.id}`)
-}
+})
 
-const deleteSubmission = (req, res) => {
+const deleteSubmission = asyncHandler(async (req, res) => {
 	res.status(200).send(`Delete Submission with id ${req.params.id}`)
-}
+})
 
 export {
 	getSubmission,
