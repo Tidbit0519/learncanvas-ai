73322571d19cb5159a03c461667bc0fae7ccbaba
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-05-29 16:45:08 -1000

    Add test api endpoint and temporarily disable MongoDB connection

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import colors from 'colors';
 
 import connectDB from './config/db.js';
 import errorHandler from './middleware/errorMIddleware.js';
 import submissionRoutes from './routes/submissionRoutes.js';
 
 dotenv.config();
-connectDB();
+// connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+
+app.get('/testAPI', (req, res) => {
+  res.status(200).send('API is working!');
+});
 
 app.use('/api/submissions', submissionRoutes);
 
