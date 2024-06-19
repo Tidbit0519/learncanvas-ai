7cfcc28a02538f0436a81a835d5ad7ea01d616c4
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-06-18 23:11:08 -1000

    Add feedback and user routes to index export

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
 export { default as submissionRouter } from "./submissionRoutes.js";
 export { default as authRouter } from "./authRoutes.js";
 export { default as userRouter } from "./userRoutes.js";
+export { default as feedbackRouter } from "./feedbackRoutes.js";
