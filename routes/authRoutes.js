5dab61965bd985697885eebb905a9d65ba8c95a8
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-06-18 23:09:52 -1000

    Add token to http only cookie

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,20 @@
 import { Router } from "express";
-import passport from "passport";
-import passportConfig from "../config/passportConfig.js";
 import jwt from "jsonwebtoken";
 import { User } from "../model/index.js";
-
-passportConfig(passport);
+import handleRefreshToken from "../controllers/refreshTokenControllers.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", async (req, res) => {
-	if (!req.body.firstname || !req.body.lastname || !req.body.email || !req.body.password)  {
+	if (
+		!req.body.firstname ||
+		!req.body.lastname ||
+		!req.body.email ||
+		!req.body.password
+	) {
 		return res.status(400).send("All fields are required");
 	}
-	const { firstname, lastname, email, password, canvasToken, role } = req.body;
+	const { firstname, lastname, email, password, role } = req.body;
 
 	try {
 		const user = await User.findOne({ email: req.body.email });
@@ -25,7 +27,6 @@ authRouter.post("/signup", async (req, res) => {
 			lastname,
 			email,
 			password,
-			canvasToken,
 			role: "user",
 		});
 		res.status(200).json({
@@ -38,34 +39,61 @@ authRouter.post("/signup", async (req, res) => {
 });
 
 authRouter.post("/login", async (req, res) => {
+	const cookies = req.cookies;
+
+	const { email, password } = req.body;
+
+	if (!email || !password)
+		return res.status(400).send("Username and password are required.");
+
 	try {
-		const user = await User.findOne({ email: req.body.email });
-		if (!user) {
+		const foundUser = await User.findOne({ email: email });
+		if (!foundUser) {
 			return res.status(401).send("Invalid email or password");
-		} else {
-			user.comparePassword(req.body.password, (err, isMatch) => {
-				if (isMatch) {
-					const tokenObj = { id: user._id, role: user.role };
-					const token = jwt.sign(tokenObj, process.env.JWT_SECRET, {
-						expiresIn: "1d",
-					});
-
-					res.cookie("jwt", token, {
-						httpOnly: true,
-						sameSite: "none",
-						secure: process.env.NODE_ENV === "production",
-						maxAge: 24 * 60 * 60 * 1000,
-					});
-
-					return res.status(200).json({ token: 'Bearer ' + token });
-				} else {
-					return res.status(401).send("Invalid email or password");
-				}
-			});
 		}
+
+		foundUser.comparePassword(req.body.password, async (err, isMatch) => {
+			if (isMatch) {
+				const tokenObj = {
+					id: foundUser._id,
+					firstname: foundUser.firstname,
+					role: foundUser.role,
+				};
+				const accessToken = jwt.sign(tokenObj, process.env.JWT_SECRET, {
+					expiresIn: "5m",
+				});
+
+				const refreshToken = jwt.sign(
+					tokenObj,
+					process.env.REFRESH_TOKEN_SECRET,
+					{
+						expiresIn: "1d",
+					}
+				);
+
+				foundUser.refreshToken = refreshToken;
+				await foundUser.save();
+
+				res.cookie("jwt", refreshToken, {
+					httpOnly: true,
+					sameSite: "None",
+					expires: new Date(Date.now() + 86400000),
+					secure: true,
+					maxAge: 86400000,
+				});
+
+				return res.status(200).json({
+					token: "Bearer " + accessToken,
+				});
+			} else {
+				return res.status(401).send("Invalid email or password");
+			}
+		});
 	} catch (error) {
 		res.status(500).send(error.message);
 	}
 });
+
+authRouter.post("/refresh", handleRefreshToken);
 
 export default authRouter;
