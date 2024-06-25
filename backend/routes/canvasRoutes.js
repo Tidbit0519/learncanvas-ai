dd394b77c2795ee6ff0cc9adceb6075d1c417e72
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-06-24 22:58:01 -1000

    Add get assignments route

--- backend/routes/canvasRoutes.js
+++ backend/routes/canvasRoutes.js
@@ -2,13 +2,12 @@ import { Router } from "express";
 import axios from "axios";
 
 const canvasRouter = Router();
+const baseUrl = process.env.CANVAS_API_URL;
+const token = "Bearer " + process.env.CANVAS_ACCESS_TOKEN;
 
-canvasRouter.get("/active", async (req, res) => {
-    const baseUrl = process.env.CANVAS_API_URL;
-    const token = "Bearer " + process.env.CANVAS_ACCESS_TOKEN;
-
-    try {
-        const response = await axios.get(
+canvasRouter.get("/activeCourses", async (req, res) => {
+	try {
+		const response = await axios.get(
 			`${baseUrl}/courses?per_page=100&enrollment_state=active`,
 			{
 				headers: {
@@ -16,8 +15,26 @@ canvasRouter.get("/active", async (req, res) => {
 				},
 			}
 		);
+		res.status(200).send(response.data);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(err);
+	}
+});
+
+canvasRouter.get("/assignments/:courseId", async (req, res) => {
+    try {
+        const response = await axios.get(
+            `${baseUrl}/courses/${req.params.courseId}/assignments?per_page=100`,
+            {
+                headers: {
+                    Authorization: token,
+                },
+            }
+        );
         res.status(200).send(response.data);
     } catch (err) {
+        console.error(err);
         res.status(500).send(err);
     }
 });
