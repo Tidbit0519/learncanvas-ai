3256df6e4a02a451bc7873a4c58f68a13750974f
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-07-12 16:32:06 -1000

    Remove filtering on assignment list

--- frontend/src/components/AssigmentList.jsx
+++ frontend/src/components/AssigmentList.jsx
@@ -4,20 +4,13 @@ import PropTypes from "prop-types";
 const AssignmentList = ({ assignments, setTutorAssignment }) => {
 	const [activeId, setActiveId] = useState(null);
 
-	const filteredAssignments = assignments.filter((assignment) => {
-		return (
-			assignment.submission_types.includes("online_upload") ||
-			assignment.submission_types.includes("online_text_entry")
-		);
-	});
-
 	const handleAssignmentClick = (id) => {
 		setActiveId((prevId) => (prevId === id ? null : id));
 	};
 
 	return (
 		<div className="flex flex-col gap-2">
-			{filteredAssignments.map((assignment) => (
+			{assignments.map((assignment) => (
 				<div
 					key={assignment.id}
 					className={`p-4 rounded-xl h-auto tracking-wider w-full hover:shadow-2xl transition-shadow duration-300 ease-in-out cursor-pointer ${
