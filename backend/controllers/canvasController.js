0bd2bd8f982b8aac67092d155c9c8884c018fdc7
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-07-17 16:49:58 -1000

    Refactor canvas routes logic into its controller

---
+++ backend/controllers/canvasController.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+const baseUrl = process.env.CANVAS_API_URL;
+const token = "Bearer " + process.env.CANVAS_ACCESS_TOKEN;
+
+const getSelf = async (req, res) => {
+    try {
+        const response = await axios.get(`${baseUrl}/users/self`, {
+            headers: {
+                Authorization: token,
+            },
+        });
+        res.status(200).send(response.data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+}
+
+const getActiveCourses = async (req, res) => {
+	try {
+		const response = await axios.get(
+			// `${baseUrl}/courses?per_page=100&enrollment_state=active`,
+			`${baseUrl}/courses/1469808`,
+			{
+				headers: {
+					Authorization: token,
+				},
+			}
+		);
+		// res.status(200).send(response.data);
+		res.status(200).send([response.data]);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(err);
+	}
+};
+
+const getAssignments = async (req, res) => {
+	try {
+		const response = await axios.get(
+			`${baseUrl}/courses/${req.params.courseId}/assignments?per_page=100`,
+			{
+				headers: {
+					Authorization: token,
+				},
+			}
+		);
+		res.status(200).send(response.data);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(err);
+	}
+};
+
+export { getSelf, getActiveCourses, getAssignments };
--- backend/routes/canvasRoutes.js
+++ backend/routes/canvasRoutes.js
@@ -1,43 +1,10 @@
 import { Router } from "express";
-import axios from "axios";
+import { getSelf, getActiveCourses, getAssignments } from "../controllers/canvasController.js";
 
 const canvasRouter = Router();
-const baseUrl = process.env.CANVAS_API_URL;
-const token = "Bearer " + process.env.CANVAS_ACCESS_TOKEN;
 
-canvasRouter.get("/activeCourses", async (req, res) => {
-	try {
-		const response = await axios.get(
-			// `${baseUrl}/courses?per_page=100&enrollment_state=active`,
-			`${baseUrl}/courses?per_page=1`,
-			{
-				headers: {
-					Authorization: token,
-				},
-			}
-		);
-		res.status(200).send(response.data);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send(err);
-	}
-});
-
-canvasRouter.get("/:courseId/assignments", async (req, res) => {
-    try {
-        const response = await axios.get(
-            `${baseUrl}/courses/${req.params.courseId}/assignments?per_page=100`,
-            {
-                headers: {
-                    Authorization: token,
-                },
-            }
-        );
-        res.status(200).send(response.data);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send(err);
-    }
-});
+canvasRouter.get("/users/self", getSelf);
+canvasRouter.get("/courses/activeCourses", getActiveCourses);
+canvasRouter.get("/courses/:courseId/assignments", getAssignments);
 
 export default canvasRouter;
