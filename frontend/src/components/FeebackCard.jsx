9d725d65d2bf40a113b12a5b87f64eb30850cafe
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-08-06 21:53:44 -1000

    Modify feedback page layout

--- frontend/src/components/FeebackCard.jsx
+++ frontend/src/components/FeebackCard.jsx
@@ -7,22 +7,21 @@ import { styles } from "../utils/styles";
 const openAPIFlag = import.meta.env.VITE_OPENAPI_FLAG;
 
 const FeedbackCard = ({ assignment, submission }) => {
-    const { description } = assignment;
-	const { loading, error, feedback, getFeedback, setFeedback } = useFeedbackApi();
+	const { description } = assignment;
+	const { loading, error, feedback, getFeedback, setFeedback } =
+		useFeedbackApi();
 	const prompt =
 		"Assignment description: " +
 		description +
 		"\n" +
 		"My submission: " +
-        submission?.body;
-    
-    const handleFeedback = async () => {
-        setFeedback("");
-        getFeedback(submission.attachments?.[0]?.url, prompt);
-    }
+		submission?.body;
 
-    console.log("openAPIFlag", openAPIFlag);
-    
+	const handleFeedback = async () => {
+		setFeedback("");
+		getFeedback(submission.attachments?.[0]?.url, prompt);
+	};
+
 	useEffect(() => {
 		if (submission && openAPIFlag) {
 			handleFeedback();
@@ -30,13 +29,7 @@ const FeedbackCard = ({ assignment, submission }) => {
 	}, [submission]);
 
 	return (
-		<div className="bg-slate-700 rounded-lg p-4 flex flex-col gap-2">
-			<h2 className={`${styles.sectionSubText}`}>Feedback</h2>
-			{error && (
-				<p className="text-red-500 bg-red-200 rounded-md p-2 text-md text-center">
-					Error loading feedback. Please try again later.
-				</p>
-			)}
+		<>
 			{submission && (
 				<p className="text-slate-400">
 					{submission.submission_type === "online_text_entry" ||
@@ -45,26 +38,33 @@ const FeedbackCard = ({ assignment, submission }) => {
 						: "Submission type not supported"}
 				</p>
 			)}
-			{loading && <p>Loading feedback...</p>}
-			{feedback && (
-				<div className="text-slate-200 flex flex-col gap-2">
-					<ReactMarkdown>{feedback}</ReactMarkdown>
+			<div className="bg-slate-700 rounded-lg p-4 flex flex-col gap-2">
+				<h2 className={`${styles.sectionSubText}`}>Feedback</h2>
+				{error && (
+					<p className="text-red-500 bg-red-200 rounded-md p-2 text-md text-center">
+						Error loading feedback. Please try again later.
+					</p>
+				)}
+
+				{loading && <p>Loading feedback...</p>}
+				{feedback && (
+					<div className="text-slate-200 flex flex-col gap-2">
+						<ReactMarkdown>{feedback}</ReactMarkdown>
+					</div>
+				)}
+				<div>
+					<button
+						className={`mt-2 p-3 py-2 ml-auto rounded-xl text-slate-100 hover:bg-violet-800 tracking-wider max-w-[200px] text-sm min-w-fit ${
+							feedback !== "" ? "bg-violet-700" : "bg-slate-500"
+						}`}
+						disabled={loading || error}
+						onClick={handleFeedback}
+					>
+						Generate Feedback
+					</button>
 				</div>
-            )}
-            <div>
-                <button
-                    className={`p-3 py-2 ml-auto rounded-xl text-slate-100 hover:bg-violet-800 tracking-wider max-w-[200px] text-sm min-w-fit ${
-                        feedback !== ""
-                            ? "bg-violet-700"
-                            : "bg-slate-500"
-                    }`}
-                    disabled={loading || error}
-                    onClick={handleFeedback}
-                >
-                    Generate Feedback
-                </button>
-            </div>
-		</div>
+			</div>
+		</>
 	);
 };
 
