0762cf25d9bf1be4112da6c57a4fccbe1d54f14f
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-10-06 23:32:07 -1000

    Add cors origin

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,12 @@ cron.schedule("0 0 * * *", async () => {
 	}
 });
 
-app.use(cors());
+app.use(cors(
+	{
+		origin: process.env.CORS_ORIGIN,
+		credentials: true,
+	}
+));
 app.use(cookieParser());
 
 app.use(bodyParser.text({ type: "text/plain" }));
