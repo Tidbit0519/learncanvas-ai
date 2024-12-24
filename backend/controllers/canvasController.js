48186e7a1328ae2a1daba510381ba5b4046f48d6
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-12-23 21:53:11 -0700

    Fix indentation

--- backend/controllers/canvasController.js
+++ backend/controllers/canvasController.js
@@ -1,91 +1,92 @@
 import axios from "axios";
 
 const getSelf = async (req, res) => {
-	try {
-		const response = await axios.get(`${req.domainUrl}/api/v1/users/self`, {
-			headers: {
-				Authorization: `Bearer ${req.canvasToken}`,
-			},
-		});
-		res.status(200).send(response.data);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send(err);
-	}
+  try {
+    const response = await axios.get(`${req.domainUrl}/api/v1/users/self`, {
+      headers: {
+        Authorization: `Bearer ${req.canvasToken}`,
+      },
+    });
+    res.status(200).send(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
 
 const getActiveCourses = async (req, res) => {
-	try {
-		const response = await axios.get(
-			`${req.domainUrl}/api/v1/courses?per_page=10&enrollment_state=active`,
-			{
-				headers: {
-					Authorization: `Bearer ${req.canvasToken}`,
-				},
-			}
-		);
-		res.status(200).send([response.data]);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send(err);
-	}
+  try {
+    const response = await axios.get(
+      `${req.domainUrl}/api/v1/courses?enrollment_state=active`,
+      {
+        headers: {
+          Authorization: `Bearer ${req.canvasToken}`,
+        },
+      }
+    );
+
+    res.status(200).send([response.data]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
 
 const getAssignments = async (req, res) => {
-	try {
-		const response = await axios.get(
-			`${req.domainUrl}/api/v1/courses/${req.params.courseId}/assignments?per_page=100`,
-			{
-				headers: {
-					Authorization: `Bearer ${req.canvasToken}`,
-				},
-			}
-		);
-		res.status(200).send(response.data);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send(err);
-	}
+  try {
+    const response = await axios.get(
+      `${req.domainUrl}/api/v1/courses/${req.params.courseId}/assignments?per_page=100`,
+      {
+        headers: {
+          Authorization: `Bearer ${req.canvasToken}`,
+        },
+      }
+    );
+    res.status(200).send(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
 
 const getAssignmentById = async (req, res) => {
-	try {
-		const response = await axios.get(
-			`${req.domainUrl}/api/v1/courses/${req.params.courseId}/assignments/${req.params.assignmentId}`,
-			{
-				headers: {
-					Authorization: `Bearer ${req.canvasToken}`,
-				},
-			}
-		);
-		res.status(200).send(response.data);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send(err);
-	}
+  try {
+    const response = await axios.get(
+      `${req.domainUrl}/api/v1/courses/${req.params.courseId}/assignments/${req.params.assignmentId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${req.canvasToken}`,
+        },
+      }
+    );
+    res.status(200).send(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
 
 const getSubmissionById = async (req, res) => {
-	try {
-		const response = await axios.get(
-			`${req.domainUrl}/api/v1/courses/${req.params.courseId}/assignments/${req.params.assignmentId}/submissions/${req.params.userId}`,
-			{
-				headers: {
-					Authorization: `Bearer ${req.canvasToken}`,
-				},
-			}
-		);
-		res.status(200).send(response.data);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send(err);
-	}
+  try {
+    const response = await axios.get(
+      `${req.domainUrl}/api/v1/courses/${req.params.courseId}/assignments/${req.params.assignmentId}/submissions/${req.params.userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${req.canvasToken}`,
+        },
+      }
+    );
+    res.status(200).send(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
 
 export {
-	getSelf,
-	getActiveCourses,
-	getAssignments,
-	getAssignmentById,
-	getSubmissionById,
+  getSelf,
+  getActiveCourses,
+  getAssignments,
+  getAssignmentById,
+  getSubmissionById,
 };
