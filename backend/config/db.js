20eedd88146021a4eb9c539c4a345b07b4b5af33
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-12-23 19:16:53 -0700

    Add database name to mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.error(`Error: ${error.message}`)
-        process.exit(1)
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName:
+        process.env.NODE_ENV === "development"
+          ? process.env.DATABASE_NAME_DEV
+          : process.env.DATABASE_NAME_PROD,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
